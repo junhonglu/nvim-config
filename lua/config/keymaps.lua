b51fbf38d0917803881ad83cc3d9f094070f8a1d
lujunhong <>
2023-02-20 22:11:43 +0800

    jj to esc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", { desc = "goto  implementation" })
+vim.keymap.set("i", "jj", "<Esc>", {})
